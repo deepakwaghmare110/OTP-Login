a785feab26f041232010cab762435e6ceeb86b64
Deepak Waghmare <deepakwaghmare110@gmail.com>
2025-07-26 15:27:45 +0530

    initial commit

--- my-react-app/src/components/PhoneLoginForm.jsx
+++ my-react-app/src/components/PhoneLoginForm.jsx
@@ -2,24 +2,41 @@ import { useState } from "react";
 
 const PhoneLoginForm = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
+  const [showOtpInput, setShowOtpInput] = useState(false);
 
   const handlePhoneNumber = (e) => setPhoneNumber(e.target.value);
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
+
+    //Phone Number Validation
+
+    const regex = /[^0-9]/g;
+    if (phoneNumber.length < 10 || regex.test(phoneNumber)) {
+      alert("Invalid Phone Number");
+      return;
+    }
+
+    setShowOtpInput(true);
   };
 
   return (
     <div>
-      <form onSubmit={handleFormSubmit}>
-        <input
-          type="text"
-          onChange={handlePhoneNumber}
-          value={phoneNumber}
-          placeholder="Enter your phone number."
-        />
-        <button type="submit">Submit</button>
-      </form>
+      {!showOtpInput ? (
+        <form onSubmit={handleFormSubmit}>
+          <input
+            type="text"
+            onChange={handlePhoneNumber}
+            value={phoneNumber}
+            placeholder="Enter your phone number."
+          />
+          <button type="submit">Submit</button>
+        </form>
+      ) : (
+        <div>
+          <p>OTP sent to {phoneNumber}</p>
+        </div>
+      )}
     </div>
   );
 };

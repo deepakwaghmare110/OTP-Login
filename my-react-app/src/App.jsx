3ee98a1e80d23599f6622eb2d8d05ed58b8219b8
Deepak Waghmare <deepakwaghmare110@gmail.com>
2025-07-26 15:00:47 +0530

    first commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import PhoneLoginForm from "./components/PhoneLoginForm";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Login with Phone</h1>
+      <PhoneLoginForm />
+    </div>
+  );
+};
+
+export default App;
---
+++ my-react-app/src/components/PhoneLoginForm.jsx
@@ -0,0 +1,11 @@
+const PhoneLoginForm = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="Enter your phone number." />
+      </form>
+    </div>
+  );
+};
+
+export default PhoneLoginForm;

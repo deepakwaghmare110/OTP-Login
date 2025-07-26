6e8ea227c4bd707fc0e09c77e3612f3e1427f2f5
Deepak Waghmare <deepakwaghmare110@gmail.com>
2025-07-26 15:12:21 +0530

    commit

--- my-react-app/src/components/PhoneLoginForm.jsx
+++ my-react-app/src/components/PhoneLoginForm.jsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
+
 const PhoneLoginForm = () => {
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const handlePhoneNumber = (e) => setPhoneNumber(e.target.value);
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div>
-      <form>
-        <input type="text" placeholder="Enter your phone number." />
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          onChange={handlePhoneNumber}
+          value={phoneNumber}
+          placeholder="Enter your phone number."
+        />
+        <button type="submit">Submit</button>
       </form>
     </div>
   );

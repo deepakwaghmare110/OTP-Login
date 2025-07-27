e1fe30575689a3061761e58be7543befba5c090b
Deepak Waghmare <deepakwaghmare110@gmail.com>
2025-07-27 23:22:59 +0530

    initial commit

--- my-react-app/src/components/OtpLogin.jsx
+++ my-react-app/src/components/OtpLogin.jsx
@@ -4,7 +4,24 @@ const OtpLogin = ({ length = 4, onOtpSubmit = () => {} }) => {
   const [otp, setOtp] = useState(new Array(length).fill(""));
   console.log(otp);
 
-  return <div></div>;
+  const handleChange = (e) => {};
+
+  return (
+    <div>
+      {otp.map((value, index) => {
+        return (
+          <input
+            key={index}
+            value={value}
+            onChange={(e) => handleChange(index, e)}
+            onClick={() => handleClick(index)}
+            onKeyDown={(e) => handleKeyDown(index, e)}
+            className="otp-input"
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default OtpLogin;

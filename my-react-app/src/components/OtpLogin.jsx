67a51bcc85522b3acad571235e3b1c4bf686951f
Deepak Waghmare <deepakwaghmare110@gmail.com>
2025-07-28 17:28:11 +0530

    initial commit

--- my-react-app/src/components/OtpLogin.jsx
+++ my-react-app/src/components/OtpLogin.jsx
@@ -2,16 +2,39 @@ import { useRef, useState, useEffect } from "react";
 
 const OtpLogin = ({ length = 4, onOtpSubmit = () => {} }) => {
   const [otp, setOtp] = useState(new Array(length).fill(""));
-  console.log(otp);
 
-  const handleChange = (e) => {};
+  const inputRefs = useRef([]); // useRef is saved in inputRefs variable
+  console.log(inputRefs);
+  // console.log(otp);
 
+  useEffect(() => {
+    if (inputRefs.current[0]) {
+      inputRefs.current[0].focus();
+    }
+  }, []);
+
+  const handleChange = (index, e) => {
+    const value = e.target.value;
+    if (isNaN(value)) return;
+
+    const newOtp = [...otp];
+
+    newOtp[index] = value.substring(value.length - 1); // 123 012  23 -1
+    setOtp(newOtp);
+
+    const combinedOtp = newOtp.join("");
+    if (combinedOtp.length === length) onOtpSubmit(combinedOtp);
+  };
+
+  const handleClick = () => {};
+  const handleKeyDown = () => {};
   return (
     <div>
       {otp.map((value, index) => {
         return (
           <input
             key={index}
+            ref={(input) => (inputRefs.current[index] = input)}
             value={value}
             onChange={(e) => handleChange(index, e)}
             onClick={() => handleClick(index)}
